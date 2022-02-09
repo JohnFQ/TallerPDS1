8404ce6ebcb0689365d5d7ca0cafa80e75ace933
juan23191 <juan_soto23191@elpoli.edu.co>
2022-02-08 23:48:35 -0500

    Cambios en ProjectTask

--- proyecto/src/main/java/co/com/poli/pds/proyectos/service/ProjectTaskServiceImpl.java
+++ proyecto/src/main/java/co/com/poli/pds/proyectos/service/ProjectTaskServiceImpl.java
@@ -4,26 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import co.com.poli.pds.proyectos.entity.BackLog;
 import co.com.poli.pds.proyectos.entity.ProjectTask;
 import co.com.poli.pds.proyectos.helper.ResponseBuilder;
 import co.com.poli.pds.proyectos.model.Response;
-import co.com.poli.pds.proyectos.repository.BackLogRepository;
 import co.com.poli.pds.proyectos.repository.ProjectTaskRepository;
-import lombok.RequiredArgsConstructor;
 
 @Service
 @CrossOrigin //Para manejar las solicitudes cruzadas que provienen del navegador del cliente
